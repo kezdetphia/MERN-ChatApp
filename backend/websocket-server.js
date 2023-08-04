47fe89bf78befcfb9a9ca7f7b0cacc659cc82dd4
kezdetphia <fehermark88@gmail.com>
2023-08-04 21:49:07 +0200

    websocket fix

--- backend/websocket-server.js
+++ backend/websocket-server.js
@@ -1,10 +1,46 @@
 const ws = require("ws");
+const jwt = require("jsonwebtoken");
+const { connection } = require("mongoose");
+const { JWT_SECRET } = process.env;
 
 const wssServer = (server) => {
-  const wss = new ws.Server({ server });
+  const wss = new ws.Server({ noServer: true });
+  const clients = new Map();
 
-  wss.on("connection", (ws) => {
+  server.on("upgrade", (req, socket, head) => {
+    // You can do additional checks here if needed, such as checking authentication or validating the connection.
+    wss.handleUpgrade(req, socket, head, (ws) => {
+      wss.emit("connection", ws, req);
+    });
+  });
+
+  wss.on("connection", (ws, req) => {
     console.log("WebSocket connected");
+    //extracting token string from header
+    const cookies = req.headers.cookie;
+    if (cookies) {
+      const tokenCookieString = cookies
+        .split(";")
+        .find((str) => str.startsWith("token="));
+      if (tokenCookieString) {
+        const token = tokenCookieString.split("=")[1];
+        if (token) {
+          jwt.verify(token, JWT_SECRET, (err, userData) => {
+            if (err) throw err;
+            //destructure id and username from userData
+            const { userId, username } = userData;
+            //storing the desctucted infromation (userData.userId, userData.username) inside the connection object userId and username key
+            //connection is inside 'wss'.client
+            connection.userId = userId;
+            connection.username = username;
+          });
+        }
+      }
+    }
+    console.log([...wss.clients].map(c=>c.username))
+ 
+
+    clients.set(ws, req); // Add the client to the clients map
 
     ws.on("message", (message) => {
       console.log("Received message", message);
@@ -12,6 +48,7 @@ const wssServer = (server) => {
 
     ws.on("close", () => {
       console.log("WebSocket connection closed");
+      clients.delete(ws); // Remove the client from the clients map when the connection is closed
     });
   });
 };
