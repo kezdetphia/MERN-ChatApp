6036a8e616fcfd3091745277b8dab7c2b6a92b4e
kezdetphia <fehermark88@gmail.com>
2023-08-14 14:41:43 +0200

    removing require .env

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 //dependencies
-require("dotenv").config();
+// require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
